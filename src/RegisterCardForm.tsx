9e7575fad229b1df94eafa4479a29a370a322e2d
Dylan Boyd <dylanjboyd@icloud.com>
2021-01-11 13:44:05 +1300

    Send to menu when clicking button

--- src/RegisterCardForm.tsx
+++ src/RegisterCardForm.tsx
@@ -5,6 +5,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,11 +21,13 @@ const useStyles = makeStyles((theme) => ({
 
 const RegisterCardForm = () => {
   const classes = useStyles();
+  const history = useHistory();
 
   return <div>
     <AppBar position="static">
       <Toolbar>
-        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu"
+                    onClick={() => history.push('/menu')}>
           <MenuIcon/>
         </IconButton>
         <Typography variant="h6" className={classes.title}>
