e5cd7d3e8892a25476243b2ce880b27a4b2b0f79
Dylan Boyd <dylanjboyd@icloud.com>
2021-01-11 19:50:01 +1300

    Add test for missing CVC

--- src/components/register/CardForm.test.tsx
+++ src/components/register/CardForm.test.tsx
@@ -16,3 +16,16 @@ test('submitting with missing card number shows error', async () => {
   // Assert
   await waitFor(() => screen.getByText('Card number must be a valid number with at least one digit.'));
 });
+
+test('submitting with missing CVC shows error', async () => {
+  // Arrange
+  renderInRouter('/');
+  userEvent.type(screen.getByLabelText('Credit card number'), '11');
+  userEvent.type(screen.getByLabelText('Expiry'), '11/1111');
+
+  // Act
+  fireEvent.click(screen.getByLabelText('submit'));
+
+  // Assert
+  await waitFor(() => screen.getByText('CVC must be a valid number.'));
+});
