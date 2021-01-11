80e5aaf0b3670a41e34cec00fab220811262c54b
Dylan Boyd <dylanjboyd@icloud.com>
2021-01-11 19:39:04 +1300

    Add test for missing card number

---
+++ src/components/register/CardForm.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { renderInRouter } from '../../test-utils';
+import userEvent from '@testing-library/user-event';
+
+test('submitting with missing card number shows error', async () => {
+  // Arrange
+  renderInRouter('/');
+  userEvent.type(screen.getByLabelText('CVC'), '11');
+  userEvent.type(screen.getByLabelText('Expiry'), '11/1111');
+
+  // Act
+  fireEvent.click(screen.getByLabelText('submit'));
+
+  // Assert
+  await waitFor(() => screen.getByText('Card number must be a valid number with at least one digit.'));
+});
