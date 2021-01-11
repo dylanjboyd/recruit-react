dd5f91e4225cc6ba86be50b7fbd4b7f0ccaa7452
Dylan Boyd <dylanjboyd@icloud.com>
2021-01-11 13:57:09 +1300

    Add credit card field

--- src/CardForm.tsx
+++ src/CardForm.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import CreditCardIcon from '@material-ui/icons/CreditCard';
 
 const CardForm = () => (
-  <div/>
+  <div>
+    <TextField label="Credit card number"
+               InputProps={{
+                 startAdornment: (
+                   <InputAdornment position="start">
+                     <CreditCardIcon/>
+                   </InputAdornment>
+                 )
+               }}/>
+  </div>
 );
 
 export default CardForm;
